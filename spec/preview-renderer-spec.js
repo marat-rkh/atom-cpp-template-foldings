00bd4f0a19159f3bae19b832c2a9919b4096ed48
Marat Habibullin <maratx387@gmail.com>
2017-06-28 20:16:24 +0300

    PreviewRenderer normalizes text now

--- spec/preview-renderer-spec.js
+++ spec/preview-renderer-spec.js
@@ -33,4 +33,32 @@ describe('PreviewRenderer', () => {
         ];
         expect(PreviewRenderer.render(params)).toEqual({ success: false });
     });
+
+    it('should replace multiple whitespaces in preview text with one whitespace', () => {
+        const params = [
+            NonTypeP('With	tab'),
+            NonTypeP('With     spaces'),
+            TypeP(/*isPack*/false, `With
+                                            newlines and spaces`, '')
+        ];
+        expect(PreviewRenderer.render(params)).toEqual({
+            success: true,
+            text: 'template<With tab, With spaces, With newlines and spaces>'
+        });
+    });
+
+    it('should create preview as one line text', () => {
+        const multilineDefaultValue =
+            `typename std::enable_if<
+                std::is_same<typename detail::decay<Other>::type, basic_tuple>::value
+            >::type`;
+        const params = [
+            TypeP(/*isPack*/false, 'Other', ''),
+            TypeP(/*isPack*/false, '', multilineDefaultValue)
+        ];
+        expect(PreviewRenderer.render(params)).toEqual({
+            success: true,
+            text: 'template<Other, _ = typename std::enable_if< std::is_same<typename detail::decay<Other>::type, basic_tuple>::value >::type>'
+        });
+    });
 });
