8a7cdeff99d4fa27f0015343b45fc7c6ada181b7
octomarat <maratx387@gmail.com>
2017-07-01 18:12:18 +0300

    Tested PseudoFolding with Windows line endings

--- spec/folding/pseudo-folding-spec.js
+++ spec/folding/pseudo-folding-spec.js
@@ -4,73 +4,76 @@ import PseudoFolding from '../../lib/folding/pseudo-folding.js';
 import TestUtils from '../test-utils.js';
 import { Range } from 'atom';
 
-fdescribe('PseudoFolding', () => {
-    it('should fold valid range with valid fold area size', () => {
-        const testDataPath = TestUtils.testDataAbsolutePath('nested_class_template.h');
-        TestUtils.withTextEditor(testDataPath, editor => {
-            const range = new Range([2, 0], [9, 1]);
-            const visualSymbolsInRange = 125; // all symbols including spaces, tabs and newlines
-            for(let size = 1; size <= visualSymbolsInRange; ++size) {
-                expect(PseudoFolding.canBeFolded(editor, range, size)).toBe(true);
-                let folding;
-                expect(() => { folding = new PseudoFolding(editor, 0, range, size) }).not.toThrow();
-                expect(editor.lineTextForScreenRow(1)).toBe('');
-                expect(TestUtils.getVisibleCharsNumberAtScreenRow(editor, 2)).toBe(size);
-                expect(editor.lineTextForScreenRow(3)).toBe('class Two');
-                folding.destroy();
-            }
+function itShouldWorkOn(testDataPath) {
+    describe(`on ${testDataPath}`, () => {
+        it('should fold valid range with valid fold area size', () => {
+            TestUtils.withTextEditor(testDataPath, editor => {
+                const range = new Range([2, 0], [9, 1]);
+                const visualSymbolsInRange = 125; // all symbols including spaces, tabs and newlines
+                for(let size = 1; size <= visualSymbolsInRange; ++size) {
+                    expect(PseudoFolding.canBeFolded(editor, range, size)).toBe(true);
+                    let folding;
+                    expect(() => { folding = new PseudoFolding(editor, 0, range, size) }).not.toThrow();
+                    expect(editor.lineTextForScreenRow(1)).toBe('');
+                    expect(TestUtils.getVisibleCharsNumberAtScreenRow(editor, 2)).toBe(size);
+                    expect(editor.lineTextForScreenRow(3)).toBe('class Two');
+                    folding.destroy();
+                }
+            });
         });
-    });
 
-    it('should fold when some folding already presents in the range', () => {
-        const testDataPath = TestUtils.testDataAbsolutePath('nested_class_template.h');
-        TestUtils.withTextEditor(testDataPath, editor => {
-            const range = new Range([2, 0], [9, 1]);
-            const size = 8;
-            for(let row = 2; row <= 9; ++row) {
-                const rowEnd = editor.clipBufferPosition([row, Infinity]);
-                editor.setSelectedBufferRange([[row, 0], rowEnd]);
-                editor.foldSelectedLines();
+        it('should fold when some folding already presents in the range', () => {
+            TestUtils.withTextEditor(testDataPath, editor => {
+                const range = new Range([2, 0], [9, 1]);
+                const size = 8;
+                for(let row = 2; row <= 9; ++row) {
+                    const rowEnd = editor.clipBufferPosition([row, Infinity]);
+                    editor.setSelectedBufferRange([[row, 0], rowEnd]);
+                    editor.foldSelectedLines();
 
-                expect(PseudoFolding.canBeFolded(editor, range, size)).toBe(true);
-                let folding;
-                expect(() => { folding = new PseudoFolding(editor, 0, range, size) }).not.toThrow();
-                expect(editor.lineTextForScreenRow(1)).toBe('');
-                expect(TestUtils.getVisibleCharsNumberAtScreenRow(editor, 2)).toBe(size);
-                expect(editor.lineTextForScreenRow(3)).toBe('class Two');
-                folding.destroy();
+                    expect(PseudoFolding.canBeFolded(editor, range, size)).toBe(true);
+                    let folding;
+                    expect(() => { folding = new PseudoFolding(editor, 0, range, size) }).not.toThrow();
+                    expect(editor.lineTextForScreenRow(1)).toBe('');
+                    expect(TestUtils.getVisibleCharsNumberAtScreenRow(editor, 2)).toBe(size);
+                    expect(editor.lineTextForScreenRow(3)).toBe('class Two');
+                    folding.destroy();
 
-                editor.unfoldAll();
-            }
+                    editor.unfoldAll();
+                }
+                for(let row = 2; row <= 8; ++row) {
+                    const rowEnd = editor.clipBufferPosition([row + 1, Infinity]);
+                    editor.setSelectedBufferRange([[row, 0], rowEnd]);
+                    editor.foldSelectedLines();
 
-            for(let row = 2; row <= 8; ++row) {
-                const rowEnd = editor.clipBufferPosition([row + 1, Infinity]);
-                editor.setSelectedBufferRange([[row, 0], rowEnd]);
-                editor.foldSelectedLines();
+                    expect(PseudoFolding.canBeFolded(editor, range, size)).toBe(true);
+                    let folding;
+                    expect(() => { folding = new PseudoFolding(editor, 0, range, size) }).not.toThrow();
+                    expect(editor.lineTextForScreenRow(1)).toBe('');
+                    expect(TestUtils.getVisibleCharsNumberAtScreenRow(editor, 2)).toBe(size);
+                    expect(editor.lineTextForScreenRow(3)).toBe('class Two');
+                    folding.destroy();
 
-                expect(PseudoFolding.canBeFolded(editor, range, size)).toBe(true);
-                let folding;
-                expect(() => { folding = new PseudoFolding(editor, 0, range, size) }).not.toThrow();
-                expect(editor.lineTextForScreenRow(1)).toBe('');
-                expect(TestUtils.getVisibleCharsNumberAtScreenRow(editor, 2)).toBe(size);
-                expect(editor.lineTextForScreenRow(3)).toBe('class Two');
-                folding.destroy();
-
-                editor.unfoldAll();
-            }
+                    editor.unfoldAll();
+                }
+            });
         });
-    });
 
-    it('should not fold invalid size', () => {
-        const testDataPath = TestUtils.testDataAbsolutePath('nested_class_template.h');
-        TestUtils.withTextEditor(testDataPath, editor => {
-            const range = new Range([2, 0], [9, 1]);
-            for(let size of [-1, 0, 126, 127]) {
-                expect(PseudoFolding.canBeFolded(editor, range, size)).toBe(false);
-                expect(() => { folding = new PseudoFolding(editor, 0, range, size) }).toThrow();
-            }
+        it('should not fold invalid size', () => {
+            TestUtils.withTextEditor(testDataPath, editor => {
+                const range = new Range([2, 0], [9, 1]);
+                for(let size of [-1, 0, 126, 127]) {
+                    expect(PseudoFolding.canBeFolded(editor, range, size)).toBe(false);
+                    expect(() => { folding = new PseudoFolding(editor, 0, range, size) }).toThrow();
+                }
+            });
         });
-    });
 
-    // TODO check invalid ranges
+        // TODO check invalid ranges
+    });
+}
+
+describe('PseudoFolding', () => {
+    itShouldWorkOn(TestUtils.testDataAbsolutePath('nested_class_template.h'));
+    itShouldWorkOn(TestUtils.testDataAbsolutePath('nested_class_template_crlf.h'));
 });
---
+++ spec/test-data/nested_class_template_crlf.h
@@ -0,0 +1,16 @@
+class One {};
+
+template<
+    typename A,
+    class B,
+    int N,
+    template<class> class TT,
+    typename C = One,
+    typename = double
+>
+class Two
+    : public One {
+    int i;
+
+    template<class Z> class Three {};
+};
