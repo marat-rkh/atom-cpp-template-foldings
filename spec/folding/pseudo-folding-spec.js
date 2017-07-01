af6acb3e66ebc54f897db9435e804df4e932c92b
Marat Habibullin <maratx387@gmail.com>
2017-07-01 09:43:19 +0300

    Unfolding is performed before PseudoFolding creation now

--- spec/folding/pseudo-folding-spec.js
+++ spec/folding/pseudo-folding-spec.js
@@ -22,5 +22,42 @@ describe('PseudoFolding', () => {
         });
     });
 
-    // TODO test folding when some folding already presents in the range
+    it('should fold when some folding already presents in the range', () => {
+        const testDataPath = TestUtils.testDataAbsolutePath('nested_class_template.h');
+        TestUtils.withTextEditor(testDataPath, editor => {
+            const range = new Range([2, 0], [9, 1]);
+            const size = 8;
+            for(let row = 2; row <= 9; ++row) {
+                const rowEnd = editor.clipBufferPosition([row, Infinity]);
+                editor.setSelectedBufferRange([[row, 0], rowEnd]);
+                editor.foldSelectedLines();
+
+                expect(PseudoFolding.canBeFolded(editor, range, size)).toBe(true);
+                let folding;
+                expect(() => { folding = new PseudoFolding(editor, 0, range, size) }).not.toThrow();
+                expect(editor.lineTextForScreenRow(1)).toBe('');
+                expect(TestUtils.getVisibleCharsNumberAtScreenRow(editor, 2)).toBe(size);
+                expect(editor.lineTextForScreenRow(3)).toBe('class Two');
+                folding.destroy();
+
+                editor.unfoldAll();
+            }
+
+            for(let row = 2; row <= 8; ++row) {
+                const rowEnd = editor.clipBufferPosition([row + 1, Infinity]);
+                editor.setSelectedBufferRange([[row, 0], rowEnd]);
+                editor.foldSelectedLines();
+
+                expect(PseudoFolding.canBeFolded(editor, range, size)).toBe(true);
+                let folding;
+                expect(() => { folding = new PseudoFolding(editor, 0, range, size) }).not.toThrow();
+                expect(editor.lineTextForScreenRow(1)).toBe('');
+                expect(TestUtils.getVisibleCharsNumberAtScreenRow(editor, 2)).toBe(size);
+                expect(editor.lineTextForScreenRow(3)).toBe('class Two');
+                folding.destroy();
+
+                editor.unfoldAll();
+            }
+        });
+    });
 });
