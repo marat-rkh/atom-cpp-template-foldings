721d10ea68bff5e8f7fc68bdfd0c84ba171930c2
octomarat <maratx387@gmail.com>
2017-07-04 16:07:15 +0300

    Notifications tests added

--- spec/atom-cpp-template-foldings-spec.js
+++ spec/atom-cpp-template-foldings-spec.js
@@ -1,73 +1,66 @@
 'use babel';
 
-import AtomCppTemplateFoldings from '../lib/atom-cpp-template-foldings';
+import TestUtils from './test-utils';
 
-// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
-//
-// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
-// or `fdescribe`). Remove the `f` to unfocus the block.
+describe('AtomCppTemplateFoldings', () => {
+    let workspaceElement, activationPromise;
 
-xdescribe('AtomCppTemplateFoldings', () => {
-  let workspaceElement, activationPromise;
-
-  beforeEach(() => {
-    workspaceElement = atom.views.getView(atom.workspace);
-    activationPromise = atom.packages.activatePackage('atom-cpp-template-foldings');
-  });
-
-  describe('when the atom-cpp-template-foldings:toggle event is triggered', () => {
-    it('hides and shows the modal panel', () => {
-      // Before the activation event the view is not on the DOM, and no panel
-      // has been created
-      expect(workspaceElement.querySelector('.atom-cpp-template-foldings')).not.toExist();
-
-      // This is an activation event, triggering it will cause the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'atom-cpp-template-foldings:toggle');
-
-      waitsForPromise(() => {
-        return activationPromise;
-      });
-
-      runs(() => {
-        expect(workspaceElement.querySelector('.atom-cpp-template-foldings')).toExist();
-
-        let atomCppTemplateFoldingsElement = workspaceElement.querySelector('.atom-cpp-template-foldings');
-        expect(atomCppTemplateFoldingsElement).toExist();
-
-        let atomCppTemplateFoldingsPanel = atom.workspace.panelForItem(atomCppTemplateFoldingsElement);
-        expect(atomCppTemplateFoldingsPanel.isVisible()).toBe(true);
-        atom.commands.dispatch(workspaceElement, 'atom-cpp-template-foldings:toggle');
-        expect(atomCppTemplateFoldingsPanel.isVisible()).toBe(false);
-      });
+    beforeEach(() => {
+        workspaceElement = atom.views.getView(atom.workspace);
+        activationPromise = atom.packages.activatePackage('atom-cpp-template-foldings');
     });
 
-    it('hides and shows the view', () => {
-      // This test shows you an integration test testing at the view level.
-
-      // Attaching the workspaceElement to the DOM is required to allow the
-      // `toBeVisible()` matchers to work. Anything testing visibility or focus
-      // requires that the workspaceElement is on the DOM. Tests that attach the
-      // workspaceElement to the DOM are generally slower than those off DOM.
-      jasmine.attachToDOM(workspaceElement);
-
-      expect(workspaceElement.querySelector('.atom-cpp-template-foldings')).not.toExist();
-
-      // This is an activation event, triggering it causes the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'atom-cpp-template-foldings:toggle');
-
-      waitsForPromise(() => {
-        return activationPromise;
-      });
-
-      runs(() => {
-        // Now we can test for view visibility
-        let atomCppTemplateFoldingsElement = workspaceElement.querySelector('.atom-cpp-template-foldings');
-        expect(atomCppTemplateFoldingsElement).toBeVisible();
-        atom.commands.dispatch(workspaceElement, 'atom-cpp-template-foldings:toggle');
-        expect(atomCppTemplateFoldingsElement).not.toBeVisible();
-      });
+    it('shows notification when only editor.softWrap option is enabled', () => {
+        const testDataPath = TestUtils.testDataAbsolutePath('nested_class_template.h');
+        TestUtils.withTextEditor(testDataPath, editor => {
+            TestUtils.withConfig({ 'editor.softWrap': true }, () => {
+                atom.commands.dispatch(workspaceElement, 'atom-cpp-template-foldings:fold-all');
+                waitsForPromise(() => { return activationPromise; });
+                runs(() => {
+                    const ns = atom.notifications.getNotifications();
+                    expect(ns.length).toBe(1);
+                    expect(ns[0].type).toBe('warning');
+                });
+            });
+        });
     });
-  });
+
+    it('shows notification when editor.softWrapAtPreferredLineLength option is enabled', () => {
+        const testDataPath = TestUtils.testDataAbsolutePath('nested_class_template.h');
+        TestUtils.withTextEditor(testDataPath, editor => {
+            const config = {
+                'editor.softWrap': true,
+                'editor.softWrapAtPreferredLineLength': true,
+                'editor.preferredLineLength': 50
+            };
+            TestUtils.withConfig(config, () => {
+                atom.commands.dispatch(workspaceElement, 'atom-cpp-template-foldings:fold-all');
+                waitsForPromise(() => { return activationPromise; });
+                runs(() => {
+                    const ns = atom.notifications.getNotifications();
+                    expect(ns.length).toBe(1);
+                    expect(ns[0].type).toBe('info');
+                });
+            });
+        });
+    });
+
+    it('does not show notification when atom-cpp-template-foldings.notifyAboutNotFoldedTemplates option is enabled', () => {
+        const testDataPath = TestUtils.testDataAbsolutePath('nested_class_template.h');
+        TestUtils.withTextEditor(testDataPath, editor => {
+            const config = {
+                'editor.softWrap': true,
+                'editor.softWrapAtPreferredLineLength': true,
+                'editor.preferredLineLength': 50,
+                'atom-cpp-template-foldings.notifyAboutNotFoldedTemplates': false
+            };
+            TestUtils.withConfig(config, () => {
+                atom.commands.dispatch(workspaceElement, 'atom-cpp-template-foldings:fold-all');
+                waitsForPromise(() => { return activationPromise; });
+                runs(() => {
+                    expect(atom.notifications.getNotifications().length).toBe(0);
+                });
+            });
+        });
+    });
 });
