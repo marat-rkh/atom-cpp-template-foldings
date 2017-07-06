039621ecc7fc02bbd860f89e7b15badeb4f9ce79
octomarat <maratx387@gmail.com>
2017-07-06 12:55:22 +0300

    Auto folding on file open added

--- spec/atom-cpp-template-foldings-spec.js
+++ spec/atom-cpp-template-foldings-spec.js
@@ -10,55 +10,86 @@ describe('AtomCppTemplateFoldings', () => {
         activationPromise = atom.packages.activatePackage('atom-cpp-template-foldings');
     });
 
-    it('shows notification when only editor.softWrap option is enabled', () => {
-        const testDataPath = TestUtils.testDataAbsolutePath('nested_class_template.h');
-        TestUtils.withTextEditor(testDataPath, editor => {
-            TestUtils.withConfig({ 'editor.softWrap': true }, () => {
-                atom.commands.dispatch(workspaceElement, 'atom-cpp-template-foldings:fold-all');
-                waitsForPromise(() => { return activationPromise; });
-                runs(() => {
-                    const ns = atom.notifications.getNotifications();
-                    expect(ns.length).toBe(1);
-                    expect(ns[0].type).toBe('warning');
+    describe('with autoFoldOnFileOpen disabled', () => {
+        let autoFoldOnFileOpen;
+        beforeEach(() => {
+            atom.config.set('atom-cpp-template-foldings.autoFoldOnFileOpen', false);
+        });
+        afterEach(() => {
+            atom.config.set('atom-cpp-template-foldings.autoFoldOnFileOpen', autoFoldOnFileOpen);
+        });
+
+        it('shows notification when only editor.softWrap option is enabled', () => {
+            const testDataPath = TestUtils.testDataAbsolutePath('nested_class_template.h');
+            TestUtils.withTextEditor(testDataPath, editor => {
+                TestUtils.withConfig({ 'editor.softWrap': true }, () => {
+                    atom.commands.dispatch(workspaceElement, 'atom-cpp-template-foldings:fold-all');
+                    waitsForPromise(() => { return activationPromise; });
+                    runs(() => {
+                        const ns = atom.notifications.getNotifications();
+                        expect(ns.length).toBe(1);
+                        expect(ns[0].type).toBe('warning');
+                    });
+                });
+            });
+        });
+
+        it('shows notification when editor.softWrapAtPreferredLineLength option is enabled', () => {
+            const testDataPath = TestUtils.testDataAbsolutePath('nested_class_template.h');
+            TestUtils.withTextEditor(testDataPath, editor => {
+                const config = {
+                    'editor.softWrap': true,
+                    'editor.softWrapAtPreferredLineLength': true,
+                    'editor.preferredLineLength': 50
+                };
+                TestUtils.withConfig(config, () => {
+                    atom.commands.dispatch(workspaceElement, 'atom-cpp-template-foldings:fold-all');
+                    waitsForPromise(() => { return activationPromise; });
+                    runs(() => {
+                        const ns = atom.notifications.getNotifications();
+                        expect(ns.length).toBe(1);
+                        expect(ns[0].type).toBe('info');
+                    });
+                });
+            });
+        });
+
+        it('does not show notification when atom-cpp-template-foldings.notifyAboutNotFoldedTemplates option is enabled', () => {
+            const testDataPath = TestUtils.testDataAbsolutePath('nested_class_template.h');
+            TestUtils.withTextEditor(testDataPath, editor => {
+                const config = {
+                    'editor.softWrap': true,
+                    'editor.softWrapAtPreferredLineLength': true,
+                    'editor.preferredLineLength': 50,
+                    'atom-cpp-template-foldings.notifyAboutNotFoldedTemplates': false
+                };
+                TestUtils.withConfig(config, () => {
+                    atom.commands.dispatch(workspaceElement, 'atom-cpp-template-foldings:fold-all');
+                    waitsForPromise(() => { return activationPromise; });
+                    runs(() => {
+                        expect(atom.notifications.getNotifications().length).toBe(0);
+                    });
                 });
             });
         });
     });
 
-    it('shows notification when editor.softWrapAtPreferredLineLength option is enabled', () => {
-        const testDataPath = TestUtils.testDataAbsolutePath('nested_class_template.h');
-        TestUtils.withTextEditor(testDataPath, editor => {
-            const config = {
-                'editor.softWrap': true,
-                'editor.softWrapAtPreferredLineLength': true,
-                'editor.preferredLineLength': 50
-            };
-            TestUtils.withConfig(config, () => {
-                atom.commands.dispatch(workspaceElement, 'atom-cpp-template-foldings:fold-all');
-                waitsForPromise(() => { return activationPromise; });
-                runs(() => {
-                    const ns = atom.notifications.getNotifications();
-                    expect(ns.length).toBe(1);
-                    expect(ns[0].type).toBe('info');
-                });
-            });
+    describe('with autoFoldOnFileOpen enabled', () => {
+        let autoFoldOnFileOpen;
+        beforeEach(() => {
+            atom.config.set('atom-cpp-template-foldings.autoFoldOnFileOpen', true);
         });
-    });
+        afterEach(() => {
+            atom.config.set('atom-cpp-template-foldings.autoFoldOnFileOpen', autoFoldOnFileOpen);
+        });
 
-    it('does not show notification when atom-cpp-template-foldings.notifyAboutNotFoldedTemplates option is enabled', () => {
-        const testDataPath = TestUtils.testDataAbsolutePath('nested_class_template.h');
-        TestUtils.withTextEditor(testDataPath, editor => {
-            const config = {
-                'editor.softWrap': true,
-                'editor.softWrapAtPreferredLineLength': true,
-                'editor.preferredLineLength': 50,
-                'atom-cpp-template-foldings.notifyAboutNotFoldedTemplates': false
-            };
-            TestUtils.withConfig(config, () => {
-                atom.commands.dispatch(workspaceElement, 'atom-cpp-template-foldings:fold-all');
-                waitsForPromise(() => { return activationPromise; });
+        it('automatically folds all templates when file opened', () => {
+            const testDataPath = TestUtils.testDataAbsolutePath('nested_class_template.h');
+            atom.commands.dispatch(workspaceElement, 'atom-cpp-template-foldings:fold-all');
+            waitsForPromise(() => { return activationPromise; });
+            TestUtils.withTextEditor(testDataPath, editor => {
                 runs(() => {
-                    expect(atom.notifications.getNotifications().length).toBe(0);
+                    expect(editor.getOverlayDecorations().length).toBe(2);
                 });
             });
         });
